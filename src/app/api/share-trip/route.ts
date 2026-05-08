2003bd567ff03d4ac2c57050498b8e28d22cf624
Michael Schramm <michael@tucan.ai>
2026-05-08 16:17:16 +0200

    fix error

--- src/app/api/share-trip/route.ts
+++ src/app/api/share-trip/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
 
   try {
     const { id, status } = await client.send({
-      to,
+      to: [to],
       template: "share-trip",
       parameters: {
         tripName,
